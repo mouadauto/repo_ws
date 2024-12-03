16ec722dffc13f3ead4faa06138113a33d223e8e
mouadauto <mouad.auto@gmail.com>
2024-12-03 15:02:59 +0100

    creation de mon appli flask initial

---
+++ app.py
@@ -0,0 +1,7 @@
+import flask
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Distant Reading Archive</h1><p>This siteis a prototype API for distant reading of science fiction novels.</p>"
+app.run(host="0.0.0.0")
